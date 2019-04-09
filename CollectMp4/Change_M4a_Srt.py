ba9add8c9d9eddb66a91208ee7c4bb409fc74156
ssf <MoonlitAlley@outlook.com>
2019-04-09 16:04:04 +0800

    Extract the audio stream form mp4 file and autosub the audio all this in python

--- CollectMp4/Change_M4a_Srt.py
+++ CollectMp4/Change_M4a_Srt.py
@@ -4,62 +4,58 @@ def GetFileList(dir , fileList , notdeal=[]):
 	newDir = dir
 	if os.path.isfile(dir):
 		#only deal with the .mp4 file
-		if os.path.splitext(dir)[1]==".mp4":
+		if os.path.splitext(dir)[1]==".m4a":
 			fileList.append(dir)
+			print "add a .m4a file to the list waiting process"
 	elif os.path.isdir(dir):
 		for s in os.listdir(dir):
 			#if there is other file unnecessary process
 			if s in notdeal:
 				continue
-
 			newDir = os.path.join(dir , s)
 			GetFileList(newDir , fileList , notdeal)
-	
+
 	return fileList #get a list contaiin the filename and filepath
 
-def moveFileToTempDir(filename):
+def moveFileToTargetDir(filename):
 	#move the file to tempdir if noexist create it
 	retname='\\\\'
 	names = filename.split('\\')
 	for i in range(len(names)-1):
 		if names[i]=="":
 			continue
-		if names[i] == "gdc":
-			retname = os.path.join(retname , names[i])
-			retname = os.path.join(retname , "gdcaudioM4adir")
+		if names[i] =="gdcaudioM4adir":
+			#skip the gdcaudioM4adir get the origin path
+			continue
 		else:
 			retname = os.path.join(retname , names[i])
 	if os.path.exists(retname)==False:
-		os.makedirs(retname)
+		return
 	retname = os.path.join(retname , names[len(names)-1])
 	#if there is a file of conflict name ,input /y parameter
 	shutil.move(filename , retname)
 
 def existsfile(filename):
-	
 	retname='\\\\'
 	names = filename.split('\\')
 	for i in range(len(names)-1):
 		if names[i]=="":
 			continue
-		if names[i] == "gdc":
-			retname = os.path.join(retname , names[i])
-			retname = os.path.join(retname , "gdcaudioM4adir")
+		if names[i] =="gdcaudioM4adir":
+			#skip the gdcaudioM4adir get the origin path
+			continue
 		else:
 			retname = os.path.join(retname , names[i])
-
-	retname = os.path.join(retname , names[len(names)-1])	
+	retname = os.path.join(retname , names[len(names)-1])
 	if os.path.isfile(retname)==False:
 		return False
 	return True
-	
 
-filepath = r'\\soft.h3d.com.cn\tac\gdc\gdc vault 2018\UX Summit\[1025099]Defining the Laws for a Parallel Reality'
+filepath = r'\\soft.h3d.com.cn\tac\gdc\gdcaudioM4adir'
 filelists = GetFileList(filepath,[],[])
 resultlists=[]
 for i in range(len(filelists)):
-	
-	#to deal with the data loacation
+	#to deal with the data
 	#resultlists.append(filelists[i].split('.')[0]) #lists store the file's whole name, use split to get the filename without it's Extension name
 
 	#because the server filesystem begin with soft.h3d.com.cn
@@ -67,7 +63,17 @@ for i in range(len(filelists)):
 	#lists store the file's whole name, use split to get the filename without it's Extension name
 
 for i in range(len(filelists)):
-	if(not existsfile(resultlists[i]+'.m4a')):
-		os.system(r'ffmpeg -i "%s" -vn -acodec copy "%s"' %(filelists[i],resultlists[i]+'.m4a')) #python's format print
-		# move the target file to temp dir
-		moveFileToTempDir(resultlists[i]+'.m4a')
+	#if there is't a Srt file in the target path
+	print "start to process the "+str(i)+"th .m4a to .Srt file"
+	if(not existsfile(resultlists[i]+'.Srt')):
+		if os.path.isfile(resultlists[i]+'.Srt') == False:
+			os.system(r'autosub "%s"' %(filelists[i])) #python's format print
+		#submit to Google Speech API get a .Srt file
+		print filelists[i]
+		#os.system(r'autosub "%s"' %(filelists[i])) #python's format print
+		#move the Srt file to origin path
+		moveFileToTargetDir(resultlists[i]+'.Srt')
+		#remove the temp audio file
+		os.remove(filelists[i])
+	else:
+		print "already be there"
